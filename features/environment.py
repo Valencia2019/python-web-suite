05f2e8f1fd5744fd4844a2faf16acd6ed4e71110
Valencia McMurray <46330428+Valencia2019@users.noreply.github.com>
2023-04-23 12:34:33 -0400

    Attempt at mobile support for locally running tests (#2)

--- features/environment.py
+++ features/environment.py
@@ -17,12 +17,19 @@ def before_all(context):
     context.base_url = "https://the-internet.herokuapp.com/"
     use_fixture(chrome_browser, context)
 
+def before_feature(context, feature):
+    if "android.device" in feature.tags:
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('androidPackage', 'com.android.chrome')
+        context.browser = webdriver.Chrome('./chromedriver', options=options)
+
 #allows user to use tags to change browser
 def before_scenario(context, scenario):
     if "chrome.browser" in scenario.tags:
         use_fixture(chrome_browser, context)
     if "safari.browser" in scenario.tags:
         use_fixture(safari_browser, context)
+
 
 #cleanup
 def after_all(context):
